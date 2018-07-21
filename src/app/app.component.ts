d2322a896d0c9d4225aab6b11478ebc6ea5f8fa8
Jeremy Kalinowski <crtjer@gmail.com>
2018-07-21 18:16:34 -0400

    Added recipe stuff.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecipesService } from './recipes-service.service';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  name = 'Serious Cooker';
+  recipesFiltered = [];
+  recipesFilteredRandomized = [];
+  loading = 'Randomizing your recipes!';
+  urlRecipe = '';
+  count = 0;
+
+  constructor(private recipeService: RecipesService) {
+  }
+
+  public ngOnInit () {
+    let recipes = this.recipeService.getRecipes();
+
+    recipes.urlset.url.forEach(recipe => {
+      if(recipe.loc.includes('/recipe')) {
+        this.recipesFiltered.push(recipe.loc);
+      }
+    });
+    this.recipesFilteredRandomized = this.shuffle(this.recipesFiltered);
+    this.loading = 'All Randomized!';
+  }
+
+  public shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+  
+  public setUrl() {
+    console.log(this.recipesFilteredRandomized[this.count]);
+    this.urlRecipe = this.recipesFilteredRandomized[this.count];
+    this.count++;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-
+import { HelloComponent } from './hello.component';
+import { RecipesService } from './recipes-service.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [RecipesService]
 })
 export class AppModule { }
